599da55251e2a7b3ccd24bda1f7988f7d50ce95f
Roneryo <roner.ortega@gmail.com>
2022-07-23 04:56:24 +0200

    push

--- js/storageHandler.js
+++ js/storageHandler.js
@@ -21,13 +21,14 @@ class miStorage{
     }
     update(data){
         this.items = JSON.parse(this.database.getItem(this.key));
-        items.push(data);
-        this.create(items)
+        this.items = this.items.filter(element=>element.id!=data.id)
+        this.items.push(data);
+        this.database.setItem(this.key,JSON.stringify(this.items));
         return 1;
     }
     findOne(id){
         this.items=JSON.parse(this.database.getItem(this.key));
-        let oneItem = this.items.find(element=>element.id===id);
+        let oneItem = this.items.find(element=>element.id==id);
         return oneItem;
     }
     getAll(){
@@ -41,33 +42,41 @@ class miStorage{
     }
 
 }
-
 //sesión, registro
 class User extends miStorage{
     constructor(
+        email,
         name,
         lastName,
-        second_name,
-        second_lastName,
+        password,
+        birthdate,
+        gender,
         id
         ){
         super(User.name);
         this.name=name;
         this.lastName=lastName;
-        this.second_name=second_name;
-        this.second_lastName=second_lastName;
+        this.password=password;
         this.id=id;
+        this.birthdate=birthdate??"";
+        this.gender=gender??"";
+        this.email=email;
         this.logged=false;
     }
     getFullName(){
-        return `${this.name} ${this.second_name} ${this.lastName} ${this.second_lastName}`
+        return `${this.name} ${this.second_name} ${this.lastName} ${this.second_lastName}`;
+        
     }
     register(){
         let registerData={
             name:this.name,
             lastName:this.lastName,
-            second_name:this.second_name,
-            second_lastName:this.second_lastName,
+        // second_name:this.second_name,
+        // second_lastName:this.second_lastName,
+            password:this.password??"",
+            birthdate:this.birthdate??"",
+            email:this.email??"",
+            gender:this.gender??"",
             id:this.id,
             logged:false
 
@@ -78,14 +87,23 @@ class User extends miStorage{
             // console.log(`la cedula ${this.id} ya se encuentra registrada`);
         }
     }
-    login(){
+    login(e){
         // console.log(this.findOne(this.id))
+    }
+    logOut(){
+        let userLoged = this.getAll();
+        if(userLoged)
+        userLoged= userLoged.filter(user=>user.logged);
+        userLoged[0].logged=false;
+        console.log(userLoged);
+        this.update(userLoged[0]);
     }
     isLogged(){
         let userLoged = this.getAll();
         if(userLoged)
         userLoged= userLoged.filter(user=>user.logged);
-        return userLoged>0;
+        console.log(userLoged)
+        return userLoged.length>0;
 
     }
 }
@@ -154,12 +172,102 @@ class Cart extends miStorage{
         super(Cart.name)
     }
 }
-let persona=new User();
-if(persona.isLogged()){
-    let productos = new Products();
-}else{
-    window.location.href="/html/login.html"
+if(window.location.pathname.split('/').find(el=>el==='index.html' )
+/*||
+window.location.pathname.split('/').find(el=>el==='mega-streaming-4k')*/){
+    let persona=new User();
+    let saludo =document.querySelector("#user");
+    let usuarioActual = persona.getAll().find(usuario=>usuario.logged);
+    console.log(usuarioActual);
+    saludo = saludo.innerHTML=`Bienvenido ${usuarioActual.name.toUpperCase()} ${usuarioActual.lastName.toUpperCase()} `
+    if(persona.isLogged()){
+        new Products();
+        let logOut = document.querySelector("#logOut");
+        logOut.addEventListener("click",(e)=>{
+            e.preventDefault();
+            persona.logOut();
+            window.location.href="/html/login.html"
+        })
+
+    }else{
+        window.location.href="/html/login.html"
+    }
 }
+if(window.location.pathname.split('/').find(el=>el==='login.html')){
+    let persona=new User();
+    if(persona.isLogged()){
+        window.location.href="/index.html"
+    }
+    let loginButton = document.querySelector("#Login");
+    loginButton.addEventListener("click",(e)=>{
+        e.preventDefault();
+        let user = document.querySelector("#usuario").value;
+        let password = document.querySelector("#password").value;
+        let getUser = new User();
+        let usuario = getUser.findOne(user);
+        console.log(getUser);
+        if(usuario){
+            if(usuario.password==password){
+                console.log("Usuario correcto")
+                usuario.logged=true;
+                getUser.update(usuario);
+                window.location.href="/index.html"
+                
+            }
+        }else{
+            console.log("Usuario o contraseña invalido")
+        }
 
+           
+    })
+    
+ 
+}
+if(window.location.pathname.split('/').find(el=>el==='registro.html')){
+    
+    console.log("registro");
 
+    let registrar = document.querySelector("#registrar");
+    registrar.addEventListener("click",(e)=>{
+        let email=document.querySelector("#correo").value;
+        let name=document.querySelector("#nombre").value;
+        let lastName=document.querySelector("#apellido").value;
+        let password=document.querySelector("#password").value;
+        let birthdate=document.querySelector("#fechadenacimiento").value;
+        let gender = document.querySelectorAll('[type="radio"]')
+        gender = [...gender].find(element=>element.checked);
+        gender=gender.value;
+        let id=document.querySelector("#cedula").value;
+    
+        e.preventDefault();
+        console.log(email,name,lastName,password,birthdate,gender,id)
+        let persona = new User(email,name,lastName,password,birthdate,gender,id)
+        persona.register()
+        persona.login()
+        window.location.href="/index.html"
+        
+        
+    })
+    // let persona = new User(
+    //     email,
+    //     name,
+    //     lastName,
+    //     password,
+    //     birthdate,
+    //     gender,
+    //     id
+    //     )
+    
+}
 /*, contacto, cuentas compradas, facutración, reportar cosgincación, reportar fallo de cuenta*/
+
+/*
+let datos ={
+    name:"Roner",
+    lasName:"Ortega",
+    password:1234,
+    id:1083051959,
+}
+let persona = new User(datos.name,datos.lastName,datos.password,datos.id)
+persona.register();
+*/
